ecc0fa5a9d36a62f6454afa30b78caf3d2d5da3c
stephen <stephen@farver.io>
2018-08-11 11:10:04 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+students = Student.create([
+  {first_name: "Alejandro", last_name: "Aguilar"},
+  {first_name: "Angela", last_name: "DiSalvo"},
+  {first_name: "Becci", last_name: "Govert"},
+  {first_name: "Cassandra", last_name: "Montoya"},
+  {first_name: "Chantal", last_name: "Wallace"},
+  {first_name: "Christopher", last_name: "Arnold"},
+  {first_name: "Cindy", last_name: "Vo"},
+  {first_name: "Daniel", last_name: "Rodriguez"},
+  {first_name: "Derek", last_name: "Silva"},
+  {first_name: "Erica", last_name: "Lehotzky"},
+  {first_name: "Erika", last_name: "Barrero"},
+  {first_name: "Jeannie", last_name: "Kim"},
+  {first_name: "Jessica", last_name: "Castaneda"},
+  {first_name: "Kelly", last_name: "Lwakatare"},
+  {first_name: "Kenny", last_name: "Yang"},
+  {first_name: "Kevin", last_name: "Peery"},
+  {first_name: "Marylene", last_name: "Chan"},
+  {first_name: "Nancy", last_name: "Do"},
+  {first_name: "Pablo", last_name: "Argueta"},
+  {first_name: "Ray", last_name: "Rodriguez"},
+  {first_name: "Ricardo", last_name: "Garza"},
+  {first_name: "Richard", last_name: "Ojo"},
+  {first_name: "Ryhan", last_name: "Chowdhury"},
+  {first_name: "Scott", last_name: "Tang"},
+  {first_name: "Scott", last_name: "Ungchusri"},
+  {first_name: "Sean", last_name: "Huang"},
+  {first_name: "Stephen", last_name: "Farver"}
+])
+
+students
